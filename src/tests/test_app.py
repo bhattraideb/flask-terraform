9c80248490d835933e50d77e3cc0a77b43de11dc
Deb Bhattrai <bhatrai.deb@gmail.com>
2023-01-02 17:16:43 +0530

    add concourse

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -1,11 +1,2 @@
-import pytest
-from web.app import app
-
-
-@pytest.fixture(name="get_client")
-def get_client():
-    return app.test_client()
-
-
-def test_app_response_hello(get_client):
-    assert get_client.get('/').text == "Hello World!"
+def test_app_response_hello(client):
+    assert client.get('/').text == "Hello World!"
